e761f32c88760ca854e2650c7b181382af12cd19
unknown <nguyenvancong2107@gmail.com>
2021-08-18 16:57:44 +0700

    upbaimoi

--- Module2/src/Bai3_Array/exercise/DeleteElement.java
+++ Module2/src/Bai3_Array/exercise/DeleteElement.java
@@ -5,19 +5,18 @@ import java.util.Scanner;
 public class DeleteElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] N = {10, 4, 6, 7, 8, 6, 0, 0, 0, 0};
+        int[] array = {10, 4, 6, 7, 8, 6, 0, 0, 0, 0};
 
         System.out.println("Delete element you want ");
         int index = Integer.parseInt(scanner.nextLine());
-        for (int i = index; i < N.length - 1; i++) {
-            N[i] = N[i + 1];
+        for (int i = index; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
         }
-
-        N[N.length - 1] = 0;
+        
+        array[array.length - 1] = 0;
         System.out.println("After delete :");
-        for (int i = 0; i < N.length; i++) {
-            System.out.print(N[i] + " ");
-
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
     }
 }
